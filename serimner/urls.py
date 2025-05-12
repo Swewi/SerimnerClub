b9fb41f77f2e15f31372f3856b0175971e1ca94e
Swewi <kireebellamy@gmail.com>
2025-05-12 11:54:47 +0200

    path fix for urls.py

--- serimner/urls.py
+++ serimner/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.conf import settings
+from django.conf.urls.static import static
 
 from home.views import home
 
