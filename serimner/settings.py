e0d40250411faa059ea0f95a70fa381ea9394ded
Swewi <kireebellamy@gmail.com>
2025-05-12 11:31:10 +0200

    update settings

--- serimner/settings.py
+++ serimner/settings.py
@@ -145,6 +145,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Cloudinary Configuration
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
+)
 
 # Static files (CSS, JavaScript, images)
 STATIC_URL = '/static/'
