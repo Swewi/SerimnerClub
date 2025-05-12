064569ff0c0d8327f6bca9fe134ad95716acb3b0
Swewi <kireebellamy@gmail.com>
2025-05-12 11:20:06 +0200

    fixing css issues

--- serimner/settings.py
+++ serimner/settings.py
@@ -63,6 +63,7 @@ INTERNAL_IPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
